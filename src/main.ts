3f6e5888984579c9a6c6c7793bdbc61bdc7e2ef0
kazimics <kazimicscn@gmail.com>
2023-06-24 00:05:28 +0800

    fix: 🐛 element button's style has been overridden

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
+import './index.css'
 import App from './App.vue'
 import router from './router/index'
 import { createPinia } from 'pinia'
-import './index.css'
 
 const app = createApp(App)
 app.use(router)
